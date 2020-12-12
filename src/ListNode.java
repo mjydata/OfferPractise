bf140fbcc47e1f436e96e80916ea6f77906f7c99
miaojunyu <miaojunyu@xiaomi.com>
2020-12-12 23:51:53 +0800

    as

---
+++ src/ListNode.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author T-Cool
+ * @date 2020/12/9 ионГ12:38
+ */
+public class ListNode {
+}
---
+++ src/leetcode/Node.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+/**
+ * @author T-Cool
+ * @date 2020/12/8 обнГ11:55
+ */
+public class Node {
+}
